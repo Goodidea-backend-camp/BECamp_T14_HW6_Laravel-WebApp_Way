9a99f6602cda6f788684c1440dc3e98723767ac0
Way <k8652090202@gmailc.com>
2025-03-26 10:58:37 +0800

    feat：首頁頁面

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+    return view('test');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dinbandon', function () {
+    return view('bandon');
+});
+
+Route::get('/meetroom', function () {
+    return view('meetroom');
+});
