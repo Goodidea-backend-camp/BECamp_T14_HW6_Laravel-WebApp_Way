d01812b3929276cd848bf38ba0a770b5a624cb2b
Way <k8652090202@gmailc.com>
2025-04-14 11:28:20 +0800

    feat:初始化資料庫

---
+++ database/migrations/0000_00_00_000006_create_meeting_records_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meeting_records', function (Blueprint $table) {
+            $table->foreignId('meeting_id')->references('id')->on('meetings')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meeting_records');
+    }
+};
