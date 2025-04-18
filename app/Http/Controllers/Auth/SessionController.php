85e74174109e16c56b39bf6b7999eaf7731d7e4c
Way <k8652090202@gmailc.com>
2025-04-18 16:14:04 +0800

    feat:登入登出後端處理

---
+++ app/Http/Controllers/Auth/SessionController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        echo "lol.index";
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'user' => ['required'],
+            'password' => ['required']
+        ]);
+        if (! Auth::attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'login' => 'Sorry, the username or the password does not match.'
+            ]);
+        }
+
+        $request->session()->regenerate();
+        $request->session()->put('user', $attributes['user']);
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy()
+    {
+        Auth::logout();
+
+        return redirect('/');
+    }
+}
