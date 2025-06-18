449238356f5b956791573f5f8f6dc55e804ed194
Way <k8652090202@gmailc.com>
2025-06-18 15:10:10 +0800

    fix: 路由dinbandon/addstores不小心刪除問題

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::middleware('auth')->prefix('meetings')->group(function () {
 Route::middleware('auth')->prefix('dinbandon')->group(function () {
     Route::get('/', [OrderController::class, 'index']);
     Route::get('addorders', [OrderController::class, 'storeInfo']);
+    Route::get('addstores', function () {
+        return view('add-store');
+    });
     Route::apiResource('stores', StoreController::class)->only(['index', 'show', 'store']);
     Route::apiResource('orders', OrderController::class)->only(['index', 'show', 'store']);
     Route::patch('orders/{id}', [OrderController::class, 'update'])->where('id', '[0-9]+');
