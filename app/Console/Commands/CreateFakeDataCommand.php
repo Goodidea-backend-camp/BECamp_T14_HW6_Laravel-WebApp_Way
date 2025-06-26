6c73a2a4d7c8db085e8d9c68336554c2ffe0fbac
Way <k8652090202@gmailc.com>
2025-06-26 18:16:55 +0800

    fix：Command檔案命名問題

--- app/Console/Commands/CreateFakeDataCommand.php
+++ app/Console/Commands/CreateFakeDataCommand.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 
-class FakeData extends Command
+class CreateFakeDataCommand extends Command
 {
     /**
      * The name and signature of the console command.
